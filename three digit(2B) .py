89db0be7fb7be482704fba52e5b677f5a2376665
mohan <112852098+mohanzeep@users.noreply.github.com>
2022-09-06 12:53:43 +0530

    Add files via upload

---
+++ three digit(2B) .py
@@ -0,0 +1,12 @@
+# paython programming find the greatest number in three digit
+print(" greatest number of three")
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+c=int(input("enter the third number:"))
+if(a<c):
+    if(a<b)and (b>c):
+        print(b,"number is greatest")
+    else:
+        print(c,"number is greatest")
+else:
+    print(a,"number is greatest")
